bd90cd923e4241bb6d6f280ab1dafcdef3b96287
Stefan Caky <stefan.caky777@gmail.com>
2021-08-01 21:16:15 +0100

    ip

--- resources/js/config/config.js
+++ resources/js/config/config.js
@@ -1,4 +1,4 @@
 export default {
-    serverTestUrl: 'http://192.168.0.21:5000/api/',
-    apiBaseUrl: 'http://192.168.0.21:5000/api/',
+    productionUrl: '',
+    apiBaseUrl: 'http://127.0.0.1:8000/api/',
   };
