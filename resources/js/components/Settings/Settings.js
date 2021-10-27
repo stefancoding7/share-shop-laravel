3aceaa79faa9c85574f756d431b2bbed68d2f13a
Stefan Caky <stefan.caky777@gmail.com>
2021-10-27 18:09:37 +0100

    added path to cors

--- resources/js/components/Settings/Settings.js
+++ resources/js/components/Settings/Settings.js
@@ -1,14 +1,33 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const Settings = (props) => {
+
+    const [name, setName] = useState('')
 
     const logout = () => {
         props.context.actions.logOut();
     };
 
+    const onChange = (e) => {
+        const { name } = e.target;
+        setName(name);
+        console.log(name)
+      }
+
     return(
         <>
-            <button onClick={logout} className="btn btn-secondary">Logout</button> 
+            <div className="settings-container m-2">
+            <label for="inputPassword5" class="form-label">Name</label>
+                <input 
+                value={name}
+                onChange={onChange}
+                type="text" id="inputPassword5" className="form-control" 
+                
+                />
+                <br />
+                <button onClick={logout} className="btn btn-secondary">Logout</button>    
+            </div>
+            
         </>
     )
 }
--- resources/js/components/Welcome/Welcome.js
+++ resources/js/components/Welcome/Welcome.js
@@ -39,20 +39,24 @@ class Welcome extends Component{
             { !loggedIn ?
             <>
               <div className="welcome-container">
-            <img className="background" src={`${config.imagesUrl}storage/site-images/welcome-image-2.jpg`} alt="..." />
+            <img className="background img-fluid" src="https://images.unsplash.com/photo-1588821323157-9fc67e64659c?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80" alt="..." />
 
             <div class="glass-container"><section className="inline">
             <section>
           
           <section className="inline">
            <h2 className="welcome-text text-center">This is a 
+
+           <dic className="text-box">
             <ReactTextTransition
-              text={texts[this.state.textFastIndex % texts.length]}
-              springConfig={presets.gentle}
-              style={{ margin: "0 4px" }}
-              inline
-              overflow
-            />
+                text={texts[this.state.textFastIndex % texts.length]}
+                springConfig={presets.gentle}
+                style={{ margin: "0 4px" }}
+                inline
+                overflow
+              />
+           </dic>
+           
             ShareShop</h2>
           </section>
         </section>
@@ -63,17 +67,10 @@ class Welcome extends Component{
 
                
             </div>
-            <div className="d-flex justify-content-around mt-3">
-                <div className="">
-                    <NavLink to="/login">
-                        <button className="shadow btn btn-outline-secondary btn-lg rounded-pill">LogIn</button>
-                    </NavLink>
-                </div>
-                <div className="">
-                    <NavLink to="/register">
-                        <button className="shadow btn btn-outline-secondary btn-lg rounded-pill">Register</button>
-                    </NavLink>
-                </div>
+            <div className="d-flex justify-content-around mt-4">
+            <h2><NavLink to="/login" className="shadow p-2 create-button rounded-pill">Create</NavLink> my first Shop List</h2>
+                 
+                
                     
                 </div>
               </>
